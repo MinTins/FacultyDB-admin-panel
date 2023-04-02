8109778d730b75b3f0e4f50be1232e43a90ea5d6
MinTins <enderator15@gmail.com>
2023-04-02 23:58:06 +0300

    Load files

---
+++ app.py
@@ -0,0 +1,391 @@
+from flask import Flask, render_template, request, redirect, url_for
+import pymysql
+
+app = Flask(__name__)
+
+# Connect to the database
+conn = pymysql.connect(
+    host="localhost",
+    user="root",
+    password="MinTnt123!",
+    database="facdb"
+)
+
+# Home page
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+# View all students
+@app.route('/students')
+def students():
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM students")
+    students = cursor.fetchall()
+    return render_template('students.html', students=students)
+
+# Add a new student
+@app.route('/students/add', methods=['GET', 'POST'])
+def add_student():
+    if request.method == 'POST':
+        name = request.form['name']
+        email = request.form['email']
+        phone = request.form['phone']
+        cursor = conn.cursor()
+        cursor.execute("INSERT INTO students (name, email, phone) VALUES (%s, %s, %s)", (name, email, phone))
+        conn.commit()
+        return redirect(url_for('students'))
+    else:
+        return render_template('add_student.html')
+
+# Edit a student
+@app.route('/students/edit/<int:id>', methods=['GET', 'POST'])
+def edit_student(id):
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM students WHERE student_id=%s", id)
+    student = cursor.fetchone()
+    if request.method == 'POST':
+        name = request.form['name']
+        email = request.form['email']
+        phone = request.form['phone']
+        cursor.execute("UPDATE students SET name=%s, email=%s, phone=%s WHERE student_id=%s", (name, email, phone, id))
+        conn.commit()
+        return redirect(url_for('students'))
+    else:
+        return render_template('edit_student.html', student=student)
+
+# Delete a student
+@app.route('/students/delete/<int:id>')
+def delete_student(id):
+    cursor = conn.cursor()
+    cursor.execute("DELETE FROM students WHERE student_id=%s", id)
+    conn.commit()
+    return redirect(url_for('students'))
+
+# Search for a student
+@app.route('/students/search', methods=['GET', 'POST'])
+def search_student():
+    if request.method == 'POST':
+        search_query = request.form['search_query']
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM students WHERE name LIKE %s OR email LIKE %s OR phone LIKE %s", ('%' + search_query + '%', '%' + search_query + '%', '%' + search_query + '%'))
+        students = cursor.fetchall()
+        return render_template('students.html', students=students)
+    else:
+        return redirect(url_for('students'))
+
+# View all courses
+@app.route('/courses')
+def courses():
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM courses")
+    courses = cursor.fetchall()
+    return render_template('courses.html', courses=courses)
+
+# Add a new course
+@app.route('/courses/add', methods=['GET', 'POST'])
+def add_course():
+    if request.method == 'POST':
+        course_name = request.form['course_name']
+        cursor = conn.cursor()
+        cursor.execute("INSERT INTO courses (course_name) VALUES (%s)", course_name)
+        conn.commit()
+        return redirect(url_for('courses'))
+    else:
+        return render_template('add_course.html')
+
+# Edit a course
+@app.route('/courses/edit/<int:id>', methods=['GET', 'POST'])
+def edit_course(id):
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM courses WHERE course_id=%s", id)
+    course = cursor.fetchone()
+    if request.method == 'POST':
+        course_name = request.form['course_name']
+        cursor.execute("UPDATE courses SET course_name=%s WHERE course_id=%s", (course_name, id))
+        conn.commit()
+        return redirect(url_for('courses'))
+    else:
+        return render_template('edit_course.html', course=course)
+
+# Delete a course
+@app.route('/courses/delete/<int:id>')
+def delete_course(id):
+    cursor = conn.cursor()
+    cursor.execute("DELETE FROM courses WHERE course_id=%s", id)
+    conn.commit()
+    return redirect(url_for('courses'))
+
+# Search for a course
+@app.route('/courses/search', methods=['GET', 'POST'])
+def search_course():
+    if request.method == 'POST':
+        search_query = request.form['search_query']
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM courses WHERE course_name LIKE %s", ('%' + search_query + '%'))
+        courses = cursor.fetchall()
+        return render_template('courses.html', courses=courses)
+    else:
+        return redirect(url_for('courses'))
+
+# View all specialties
+@app.route('/specialties')
+def specialties():
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM specialties")
+    specialties = cursor.fetchall()
+    return render_template('specialties.html', specialties=specialties)
+
+# Add a new specialty
+@app.route('/specialties/add', methods=['GET', 'POST'])
+def add_specialty():
+    if request.method == 'POST':
+        specialty_name = request.form['specialty_name']
+        faculty_id = request.form['faculty_id']
+        cursor = conn.cursor()
+        cursor.execute("INSERT INTO specialties (specialty_name, faculty_id) VALUES (%s, %s)", (specialty_name, faculty_id))
+        conn.commit()
+        return redirect(url_for('specialties'))
+    else:
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM faculties")
+        faculties = cursor.fetchall()
+        return render_template('add_specialty.html', faculties=faculties)
+
+# Edit a specialty
+@app.route('/specialties/edit/<int:id>', methods=['GET', 'POST'])
+def edit_specialty(id):
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM specialties WHERE specialty_id=%s", id)
+    specialty = cursor.fetchone()
+    if request.method == 'POST':
+        specialty_name = request.form['specialty_name']
+        faculty_id = request.form['faculty_id']
+        cursor.execute("UPDATE specialties SET specialty_name=%s, faculty_id=%s WHERE specialty_id=%s", (specialty_name, faculty_id, id))
+        conn.commit()
+        return redirect(url_for('specialties'))
+    else:
+        cursor.execute("SELECT * FROM faculties")
+        faculties = cursor.fetchall()
+        return render_template('edit_specialty.html', specialty=specialty, faculties=faculties)
+
+# Delete a specialty
+@app.route('/specialties/delete/<int:id>')
+def delete_specialty(id):
+    cursor = conn.cursor()
+    cursor.execute("DELETE FROM specialties WHERE specialty_id=%s", id)
+    conn.commit()
+    return redirect(url_for('specialties'))
+
+# Search for a specialty
+@app.route('/specialties/search', methods=['GET', 'POST'])
+def search_specialty():
+    if request.method == 'POST':
+        search_query = request.form['search_query']
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM specialties WHERE specialty_name LIKE %s", ('%' + search_query + '%'))
+        specialties = cursor.fetchall()
+        return render_template('specialties.html', specialties=specialties)
+    else:
+        return redirect(url_for('specialties'))
+
+# View all groups
+@app.route('/groups')
+def groups():
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM groups_list")
+    groups = cursor.fetchall()
+    return render_template('groups.html', groups=groups)
+
+# Add a new group
+@app.route('/groups/add', methods=['GET', 'POST'])
+def add_group():
+    if request.method == 'POST':
+        group_name = request.form['group_name']
+        course_id = request.form['course_id']
+        specialty_id = request.form['specialty_id']
+        cursor = conn.cursor()
+        cursor.execute("INSERT INTO groups_list (group_name, course_id, specialty_id) VALUES (%s, %s, %s)", (group_name, course_id, specialty_id))
+        conn.commit()
+        return redirect(url_for('groups'))
+    else:
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM courses")
+        courses = cursor.fetchall()
+        cursor.execute("SELECT * FROM specialties")
+        specialties = cursor.fetchall()
+        return render_template('add_group.html', courses=courses, specialties=specialties)
+
+# Edit a group
+@app.route('/groups/edit/<int:id>', methods=['GET', 'POST'])
+def edit_group(id):
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM groups_list WHERE group_id=%s", id)
+    group = cursor.fetchone()
+    if request.method == 'POST':
+        group_name = request.form['group_name']
+        course_id = request.form['course_id']
+        specialty_id = request.form['specialty_id']
+        cursor.execute("UPDATE groups_list SET group_name=%s, course_id=%s, specialty_id=%s WHERE group_id=%s", (group_name, course_id, specialty_id, id))
+        conn.commit()
+        return redirect(url_for('groups'))
+    else:
+        cursor.execute("SELECT * FROM courses")
+        courses = cursor.fetchall()
+        cursor.execute("SELECT * FROM specialties")
+        specialties = cursor.fetchall()
+        return render_template('edit_group.html', group=group, courses=courses, specialties=specialties)
+
+# Delete a group
+@app.route('/groups/delete/<int:id>')
+def delete_group(id):
+    cursor = conn.cursor()
+    cursor.execute("DELETE FROM groups_list WHERE group_id=%s", id)
+    conn.commit()
+    return redirect(url_for('groups'))
+
+# Search for a group
+@app.route('/groups/search', methods=['GET', 'POST'])
+def search_group():
+    if request.method == 'POST':
+        search_query = request.form['search_query']
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM groups_list WHERE group_name LIKE %s", ('%' + search_query + '%'))
+        groups = cursor.fetchall()
+        return render_template('groups.html', groups=groups)
+    else:
+        return redirect(url_for('groups'))
+
+
+# View all faculties
+@app.route('/faculties')
+def faculties():
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM faculties")
+    faculties = cursor.fetchall()
+    return render_template('faculties.html', faculties=faculties)
+
+
+# Add a new faculty
+@app.route('/faculties/add', methods=['GET', 'POST'])
+def add_faculty():
+    if request.method == 'POST':
+        faculty_name = request.form['faculty_name']
+        cursor = conn.cursor()
+        cursor.execute("INSERT INTO faculties (faculty_name) VALUES (%s)", faculty_name)
+        conn.commit()
+        return redirect(url_for('faculties'))
+    else:
+        return render_template('add_faculty.html')
+
+
+# Edit a faculty
+@app.route('/faculties/edit/<int:id>', methods=['GET', 'POST'])
+def edit_faculty(id):
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM faculties WHERE faculty_id=%s", id)
+    faculty = cursor.fetchone()
+    if request.method == 'POST':
+        faculty_name = request.form['faculty_name']
+        cursor.execute("UPDATE faculties SET faculty_name=%s WHERE faculty_id=%s", (faculty_name, id))
+        conn.commit()
+        return redirect(url_for('faculties'))
+    else:
+        return render_template('edit_faculty.html', faculty=faculty)
+
+
+# Delete a faculty
+@app.route('/faculties/delete/<int:id>')
+def delete_faculty(id):
+    cursor = conn.cursor()
+    cursor.execute("DELETE FROM faculties WHERE faculty_id=%s", id)
+    conn.commit()
+    return redirect(url_for('faculties'))
+
+
+# Search for a faculty
+@app.route('/faculties/search', methods=['GET', 'POST'])
+def search_faculty():
+    if request.method == 'POST':
+        search_query = request.form['search_query']
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM faculties WHERE faculty_name LIKE %s", ('%' + search_query + '%'))
+        faculties = cursor.fetchall()
+        return render_template('faculties.html', faculties=faculties)
+    else:
+        return redirect(url_for('faculties'))
+
+
+# View all student groups
+@app.route('/student_groups')
+def student_groups():
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM student_group")
+    student_groups = cursor.fetchall()
+    return render_template('student_groups.html', student_groups=student_groups)
+
+
+# Add a new student group
+@app.route('/student_groups/add', methods=['GET', 'POST'])
+def add_student_group():
+    if request.method == 'POST':
+        student_id = request.form['student_id']
+        group_id = request.form['group_id']
+        cursor = conn.cursor()
+        cursor.execute("INSERT INTO student_group (student_id, group_id) VALUES (%s, %s)", (student_id, group_id))
+        conn.commit()
+        return redirect(url_for('student_groups'))
+    else:
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM students")
+        students = cursor.fetchall()
+        cursor.execute("SELECT * FROM groups_list")
+        groups = cursor.fetchall()
+        return render_template('add_student_group.html', students=students, groups=groups)
+
+
+# Edit a student group
+@app.route('/student_groups/edit/<int:id>', methods=['GET', 'POST'])
+def edit_student_group(id):
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM student_group WHERE id=%s", id)
+    student_group = cursor.fetchone()
+    if request.method == 'POST':
+        student_id = request.form['student_id']
+        group_id = request.form['group_id']
+        cursor.execute("UPDATE student_group SET student_id=%s, group_id=%s WHERE id=%s", (student_id, group_id, id))
+        conn.commit()
+        return redirect(url_for('student_groups'))
+    else:
+        cursor.execute("SELECT * FROM students")
+        students = cursor.fetchall()
+        cursor.execute("SELECT * FROM groups_list")
+        groups = cursor.fetchall()
+        return render_template('edit_student_group.html', student_group=student_group, students=students, groups=groups)
+
+
+# Delete a student group
+@app.route('/student_groups/delete/<int:id>')
+def delete_student_group(id):
+    cursor = conn.cursor()
+    cursor.execute("DELETE FROM student_group WHERE id=%s", id)
+    conn.commit()
+    return redirect(url_for('student_groups'))
+
+
+# Search for a student group
+@app.route('/student_groups/search', methods=['GET', 'POST'])
+def search_student_group():
+    if request.method == 'POST':
+        search_query = request.form['search_query']
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM student_group WHERE student_id LIKE %s OR group_id LIKE %s", ('%' + search_query + '%', '%' + search_query + '%'))
+        student_groups = cursor.fetchall()
+        return render_template('student_groups.html', student_groups=student_groups)
+    else:
+        return redirect(url_for('student_groups'))
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ dump.sql
@@ -0,0 +1,143 @@
+-- MySQL dump 10.13  Distrib 8.0.32, for Win64 (x86_64)
+--
+-- Host: localhost    Database: facdb
+-- ------------------------------------------------------
+-- Server version	8.0.32
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `courses`
+--
+
+DROP TABLE IF EXISTS `courses`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `courses` (
+  `course_id` int NOT NULL AUTO_INCREMENT,
+  `course_name` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`course_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `faculties`
+--
+
+DROP TABLE IF EXISTS `faculties`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `faculties` (
+  `faculty_id` int NOT NULL AUTO_INCREMENT,
+  `faculty_name` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`faculty_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `groups_list`
+--
+
+DROP TABLE IF EXISTS `groups_list`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `groups_list` (
+  `group_id` int NOT NULL AUTO_INCREMENT,
+  `group_name` varchar(50) DEFAULT NULL,
+  `course_id` int DEFAULT NULL,
+  `specialty_id` int DEFAULT NULL,
+  PRIMARY KEY (`group_id`),
+  KEY `course_id` (`course_id`),
+  KEY `specialty_id` (`specialty_id`),
+  CONSTRAINT `groups_list_ibfk_1` FOREIGN KEY (`course_id`) REFERENCES `courses` (`course_id`),
+  CONSTRAINT `groups_list_ibfk_2` FOREIGN KEY (`specialty_id`) REFERENCES `specialties` (`specialty_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `specialties`
+--
+
+DROP TABLE IF EXISTS `specialties`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `specialties` (
+  `specialty_id` int NOT NULL AUTO_INCREMENT,
+  `specialty_name` varchar(50) DEFAULT NULL,
+  `faculty_id` int DEFAULT NULL,
+  PRIMARY KEY (`specialty_id`),
+  KEY `faculty_id` (`faculty_id`),
+  CONSTRAINT `specialties_ibfk_1` FOREIGN KEY (`faculty_id`) REFERENCES `faculties` (`faculty_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `student_group`
+--
+
+DROP TABLE IF EXISTS `student_group`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `student_group` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `student_id` int DEFAULT NULL,
+  `group_id` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `student_id` (`student_id`),
+  KEY `group_id` (`group_id`),
+  CONSTRAINT `student_group_ibfk_1` FOREIGN KEY (`student_id`) REFERENCES `students` (`student_id`),
+  CONSTRAINT `student_group_ibfk_2` FOREIGN KEY (`group_id`) REFERENCES `groups_list` (`group_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+--
+-- Table structure for table `students`
+--
+
+DROP TABLE IF EXISTS `students`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `students` (
+  `student_id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) DEFAULT NULL,
+  `email` varchar(50) DEFAULT NULL,
+  `phone` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`student_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb3 */ ;
+/*!50003 SET character_set_results = utf8mb3 */ ;
+/*!50003 SET collation_connection  = utf8mb3_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'NO_AUTO_VALUE_ON_ZERO' */ ;
+DELIMITER ;;
+/*!50003 CREATE*/ /*!50017 DEFINER=`root`@`localhost`*/ /*!50003 TRIGGER `students_before_insert` BEFORE INSERT ON `students` FOR EACH ROW BEGIN
+    SET NEW.phone = REPLACE(NEW.phone, ' ', '');
+END */;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-04-02 22:33:14
